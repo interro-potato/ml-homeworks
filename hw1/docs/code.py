f1b8a424cc67b5200e4c234ba0ca2ac7fa41584b
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-10-06 16:40:33 +0100

    Update plot generation using SelectKBest

--- hw1/docs/code.py
+++ hw1/docs/code.py
@@ -7,9 +7,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import SelectKBest
 
 FEATURE_AMOUNT = [5, 10, 40, 100, 250, 700]
 SEED = 1
+SPLIT = 0.3
 
 # Loading data
 
@@ -20,35 +22,35 @@ df['class'] = df['class'].str.decode('utf-8')
 # The first step should be splitting the dataset into stratified training and
 # testing sets (70-30 split) with a fixed seed (random_state=1).
 
-train, test = train_test_split(df, test_size=0.3, random_state=SEED, stratify=df['class'])
-X = train.drop('class', axis=1)
-y = train['class']
-
-# Afterward, we should try to check the discriminative power of each feature considering the mutual_info_classif criterion.
-# If we can only use a subset of features, we should select the most discriminative ones,
-# (i.e, if we can use 5 features, we should select the 5 most discriminative ones, and so on).
-
-mimportance = mutual_info_classif(X, y, random_state=SEED)
-SORTED_FEATURE_INDICES = np.argsort(mimportance)[::-1]
+X, y = df.drop('class', axis=1), df['class']
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y,
+  test_size=SPLIT,
+  random_state=SEED,
+  stratify=y
+)
 
 # Now, time to finally assess (in a single plot) both the training and testing accuracies
 # of a decision tree with no depth limits (and remaining default behavior) for a varying number of selected features.
 
+CLF = DecisionTreeClassifier(random_state=SEED)
 def train_and_test(X_train, X_test, y_train, y_test):
-  clf = DecisionTreeClassifier(random_state=SEED)
-  clf.fit(X_train, y_train)
-  y_pred = clf.predict(X_test)
-  return accuracy_score(y_test, y_pred)
+    CLF.fit(X_train, y_train)
+    y_pred = CLF.predict(X_test)
+    return accuracy_score(y_test, y_pred)
+def mutual_info_classif_custom(X, y):
+    return mutual_info_classif(X, y, random_state=SEED)
 
 train_accuracies = []
 test_accuracies = []
 
 for n in FEATURE_AMOUNT:
-    chosen_features = X.columns[SORTED_FEATURE_INDICES[:n]]
-    X_train = train[chosen_features]
-    X_test = test[chosen_features]
-    train_accuracies.append(train_and_test(X_train, X_train, y, y))
-    test_accuracies.append(train_and_test(X_train, X_test, y, test['class']))
+    selector = SelectKBest(score_func=mutual_info_classif_custom, k=n)
+    selector.fit(X_train, y_train)
+    X_train_new = selector.transform(X_train)
+    X_test_new = selector.transform(X_test)
+    train_accuracies.append(train_and_test(X_train_new, X_train_new, y_train, y_train))
+    test_accuracies.append(train_and_test(X_train_new, X_test_new, y_train, y_test))
 
 plt.plot(FEATURE_AMOUNT, train_accuracies, label='Training Accuracy')
 plt.plot(FEATURE_AMOUNT, test_accuracies, label='Testing Accuracy')
