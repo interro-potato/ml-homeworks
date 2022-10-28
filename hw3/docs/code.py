a5162c48ace9dbfe2b2efc307693133f5bba18ae
Rafael Oliveira <rafdev.x@gmail.com>
2022-10-28 22:23:53 +0100

    adjust code style to better fit pdf

--- hw3/docs/code.py
+++ hw3/docs/code.py
@@ -17,80 +17,86 @@ df = pd.DataFrame(data[0])
 X = df.drop("y", axis=1).values
 y = df["y"].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y,
+  test_size=0.3, random_state=0
+)
 
 
 def predict(regressor):
-    regressor.fit(X_train, y_train)
-    return regressor.predict(X_test)
+  regressor.fit(X_train, y_train)
+  return regressor.predict(X_test)
 
 
 def plot_regressors_residues():
-    """Utilized for answering question 5."""
-    global regressors
-    predictions = []
-    descriptions = []
-    for description, regressor in regressors.items():
-        predictions.append(predict(regressor))
-        descriptions.append(description)
+  """Utilized for answering question 5."""
+  global regressors
+  predictions = []
+  descriptions = []
+  for description, regressor in regressors.items():
+    predictions.append(predict(regressor))
+    descriptions.append(description)
 
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(8, 12))
+  fig, (ax1, ax2) = plt.subplots(2, figsize=(8, 12))
 
-    data = pd.DataFrame(
-        {
-            description: np.abs(y_test - prediction)
-            for description, prediction in zip(descriptions, predictions)
-        }
-    )
+  data = pd.DataFrame(
+    {
+      description: np.abs(y_test - prediction)
+      for description, prediction in zip(descriptions, predictions)
+    }
+  )
 
-    sns.histplot(
-        data=data,
-        bins=30,
-        ax=ax1,
-        multiple="dodge",
-    )
-    ax1.set_title("Residues histogram for each regressor")
-    ax1.set_xlabel("Residue")
-    ax1.set_ylabel("Count")
-    ax1.legend(descriptions)
+  sns.histplot(
+    data=data,
+    bins=30,
+    ax=ax1,
+    multiple="dodge",
+  )
+  ax1.set_title("Residues histogram for each regressor")
+  ax1.set_xlabel("Residue")
+  ax1.set_ylabel("Count")
+  ax1.legend(descriptions)
 
-    sns.boxplot(
-        data=data,
-        ax=ax2,
-        orient="h",
-    )
-    ax2.set_title("Residues boxplot for each regressor")
-    ax2.set_xlabel("Residue")
-    ax2.set_ylabel("Regressor")
-    ax2.legend(descriptions)
-    plt.savefig("assets/residues.png")
+  sns.boxplot(
+    data=data,
+    ax=ax2,
+    orient="h",
+  )
+  ax2.set_title("Residues boxplot for each regressor")
+  ax2.set_xlabel("Residue")
+  ax2.set_ylabel("Regressor")
+  ax2.legend(descriptions)
+  plt.savefig("assets/residues.png")
 
 
 def print_regressor(regressor, description, y_pred):
-    """Utilized for answering questions 4. and 6."""
-    print(description)
-    print("MAE: {:.5f}".format(mean_absolute_error(y_test, y_pred)))
-    if "MLP" in description:
-        print("Iterations: {}".format(regressor.n_iter_))
+  """Utilized for answering questions 4. and 6."""
+  print(description)
+  print("MAE: {:.5f}".format(mean_absolute_error(y_test, y_pred)))
+  if "MLP" in description:
+    print("Iterations: {}".format(regressor.n_iter_))
 
 
 regressors = {
-    "Ridge": Ridge(alpha=0.1),
-    "MLP 1": MLPRegressor(
-        hidden_layer_sizes=(10, 10),
-        activation="tanh",
-        max_iter=500,
-        random_state=0,
-        early_stopping=True,
-    ),
-    "MLP 2": MLPRegressor(
-        hidden_layer_sizes=(10, 10), activation="tanh", max_iter=500, random_state=0
-    ),
+  "Ridge": Ridge(alpha=0.1),
+  "MLP 1": MLPRegressor(
+    hidden_layer_sizes=(10, 10),
+    activation="tanh",
+    max_iter=500,
+    random_state=0,
+    early_stopping=True,
+  ),
+  "MLP 2": MLPRegressor(
+    hidden_layer_sizes=(10, 10),
+    activation="tanh",
+    max_iter=500,
+    random_state=0
+  ),
 }
 
 for description, regressor in regressors.items():
-    y_pred = predict(regressor)
-    print("---")
-    print_regressor(regressor, description, y_pred)
+  y_pred = predict(regressor)
+  print("---")
+  print_regressor(regressor, description, y_pred)
 
 plot_regressors_residues()
